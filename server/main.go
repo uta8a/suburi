75a78ab9e2ece375fc301cc84d0df97469195f56
uta8a <noumodulity@gmail.com>
2020-07-17 21:28:22 +0900

    server

--- server/main.go
+++ server/main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-  pb "suburi/server/helloworld"
-  server "suburi/server/handler"
-  "log"
-  "net"
+	"log"
+	"net"
+	server "suburi/server/handler"
+	pb "suburi/server/helloworld"
 
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/reflection"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 const (
-  port = ":9090"
+	port = ":9090"
 )
 
 func main() {
-  lis, err := net.Listen("tcp", port)
-  if err != nil {
-    log.Fatalf("failed to listen: %v", err)
-  }
-  s := grpc.NewServer()
-  pb.RegisterGreeterServer(s, server.NewApp())
-  // reflection
-  reflection.Register(s)
-  if err := s.Serve(lis); err != nil {
-    log.Fatalf("failed to serve: %v", err)
-  }
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, server.NewApp())
+	// reflection
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
