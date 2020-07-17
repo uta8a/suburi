66929497ee56ba42729d192d4bc8efcdc29a408f
uta8a <noumodulity@gmail.com>
2020-07-17 11:01:07 +0900

    delete server init

--- server/main.go
+++ server/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
   pb "suburi/server/helloworld"
+  server "suburi/server/handler"
   "log"
   "net"
 
-  "golang.org/x/net/context"
   "google.golang.org/grpc"
   "google.golang.org/grpc/reflection"
 )
@@ -14,20 +14,13 @@ const (
   port = ":9090"
 )
 
-// server for helloworld.GreeterServer
-type server struct{}
-
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-  return &pb.HelloReply{Message: "Hello " + in.Name}, nil
-}
-
 func main() {
   lis, err := net.Listen("tcp", port)
   if err != nil {
     log.Fatalf("failed to listen: %v", err)
   }
   s := grpc.NewServer()
-  pb.RegisterGreeterServer(s, &server{})
+  pb.RegisterGreeterServer(s, server.NewApp())
   // reflection
   reflection.Register(s)
   if err := s.Serve(lis); err != nil {
