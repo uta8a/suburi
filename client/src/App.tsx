561603e22befb1facd8dc20f72ee0156cc72759b
uta8a <noumodulity@gmail.com>
2020-07-19 06:42:01 +0900

    embed bearer -> ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -26,9 +26,11 @@ const App = () => {
   const onClick = () => {
     const request = new TellMeSecretRequest();
     request.setMessage(inputText);
-
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhbGljZSIsIm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0.vqbJV8ftQ1Tx_UMqUioMd5jEve9Odpw9--NJLsQOSeo';
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJib2IiLCJpYXQiOjE1MTYyMzkwMjJ9.YXGHLm79VdQH5Ccre7MxKXHTcbDMb4sz2uYqiQE21Vg';
+    const metadata = {'authorization': `bearer ${token}`};
     const client = new GreeterClient(`${URL}`, {}, {});
-    client.tellMeSecret(request, {}, (err, ret) => {
+    client.tellMeSecret(request, metadata, (err, ret) => {
       if (err || ret == null) {
         throw err;
       }
