d54b01ed16c4b8e475c1ca05d52cb7e257fbe1e5
uta8a <noumodulity@gmail.com>
2020-07-20 06:06:04 +0900

    change gen.sh

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-CLIENT_OUTDIR=client/src/helloworld
-SERVER_OUTPUT_DIR=server/helloworld
+CLIENT_OUTDIR=client/src/user
+SERVER_OUTPUT_DIR=server/user
 
 mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
 
@@ -10,8 +10,8 @@ mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
 #  --grpc-web_out => helloworld_pb.d.ts
 #  --grpc-web_out => HelloworldServiceClientPb.ts
 #  --go_out => helloworld.pb.go
-protoc --proto_path=protocol helloworld.proto \
+protoc --proto_path=internal/proto/user user.proto \
     --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
     --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
     --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
-sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/helloworld_pb.js
+sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/user_pb.js
