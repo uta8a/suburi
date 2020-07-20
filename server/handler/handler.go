e65d0bbc89e1bdbacfb6b22f14db07bcdea5c9bf
uta8a <noumodulity@gmail.com>
2020-07-20 10:05:23 +0900

    template for method

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -8,10 +8,33 @@ import (
 
 type app struct {}
 
+const (
+  GetTokenSuccessMessage = "This is your token."
+  GetTokenErrorMessage = ""
+  LoginSuccessMessage = "Login Success"
+  RegisterSuccessMessage = "Register Success"
+  LogoutSuccessMessage = "Logout Success"
+)
+
 func NewApp() *app {
   return &app{}
 }
 
 func (s *app) GetToken(ctx context.Context, req *pbuser.Request) (*pbuser.Result, error) {
-  return &pbuser.Result{ Token: "xxxyyyzzz" }, nil
+  // return sample token for debug
+  return &pbuser.Result{ Token: "xxx", DisplayMessage: GetTokenSuccessMessage }, nil
 }
+func (s *app) Login(ctx context.Context, req *pbuser.Request) (*pbuser.Result, error) {
+  // DB? or Auth Server
+  return &pbuser.Result{ Token: "xxx", DisplayMessage: LoginSuccessMessage }, nil
+}
+func (s *app) Register(ctx context.Context, req *pbuser.Request) (*pbuser.Result, error) {
+  // nearly equal Login
+  return &pbuser.Result{ Token: "xxx", DisplayMessage: RegisterSuccessMessage }, nil
+}
+func (s *app) Logout(ctx context.Context, req *pbuser.Request) (*pbuser.Result, error) {
+  // delete token
+  return &pbuser.Result{ Token: "xxx", DisplayMessage: LogoutSuccessMessage }, nil
+}
+
+
