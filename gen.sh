f118bddf3192e6575895786035cce5859dd108b2
uta8a <noumodulity@gmail.com>
2020-07-20 22:57:21 +0900

    modify gen.sh

--- gen.sh
+++ gen.sh
@@ -5,13 +5,52 @@ SERVER_OUTPUT_DIR=server/proto/user
 
 mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
 
-# protocol/helloworld.proto
-#  --js_out => helloworld_pb.js
-#  --grpc-web_out => helloworld_pb.d.ts
-#  --grpc-web_out => HelloworldServiceClientPb.ts
-#  --go_out => helloworld.pb.go
 protoc --proto_path=internal/proto/user user.proto \
     --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
     --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
     --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
 sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/user_pb.js
+
+CLIENT_OUTDIR=client/src/proto/challenge
+SERVER_OUTPUT_DIR=server/proto/challenge
+
+mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
+
+protoc --proto_path=internal/proto/challenge challenge.proto \
+    --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
+    --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
+    --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
+sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/challenge_pb.js
+
+CLIENT_OUTDIR=client/src/proto/check
+SERVER_OUTPUT_DIR=server/proto/check
+
+mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
+
+protoc --proto_path=internal/proto/check check.proto \
+    --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
+    --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
+    --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
+sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/check_pb.js
+
+CLIENT_OUTDIR=client/src/proto/id
+SERVER_OUTPUT_DIR=server/proto/id
+
+mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
+
+protoc --proto_path=internal/proto/id id.proto \
+    --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
+    --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
+    --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
+sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/id_pb.js
+
+CLIENT_OUTDIR=client/src/proto/scoreboard
+SERVER_OUTPUT_DIR=server/proto/scoreboard
+
+mkdir -p ${CLIENT_OUTDIR} ${SERVER_OUTPUT_DIR}
+
+protoc --proto_path=internal/proto/scoreboard scoreboard.proto \
+    --js_out=import_style=commonjs:${CLIENT_OUTDIR} \
+    --grpc-web_out=import_style=typescript,mode=grpcwebtext:${CLIENT_OUTDIR} \
+    --go_out=plugins=grpc:${SERVER_OUTPUT_DIR}
+sed -i '1i/* eslint-disable */' ${CLIENT_OUTDIR}/scoreboard_pb.js
