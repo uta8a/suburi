e3dd267bcf1835810296c8294e346a77dceb5b2f
uta8a <noumodulity@gmail.com>
2020-07-20 22:57:08 +0900

    Auto generated files

--- client/src/proto/check/check_pb.d.ts
+++ client/src/proto/check/check_pb.d.ts
@@ -2,3 +2,39 @@ import * as jspb from 'google-protobuf'
 
 
 
+export class Request extends jspb.Message {
+  getNone(): string;
+  setNone(value: string): Request;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Request.AsObject;
+  static toObject(includeInstance: boolean, msg: Request): Request.AsObject;
+  static serializeBinaryToWriter(message: Request, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Request;
+  static deserializeBinaryFromReader(message: Request, reader: jspb.BinaryReader): Request;
+}
+
+export namespace Request {
+  export type AsObject = {
+    none: string,
+  }
+}
+
+export class Response extends jspb.Message {
+  getDisplayMessage(): string;
+  setDisplayMessage(value: string): Response;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): Response.AsObject;
+  static toObject(includeInstance: boolean, msg: Response): Response.AsObject;
+  static serializeBinaryToWriter(message: Response, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): Response;
+  static deserializeBinaryFromReader(message: Response, reader: jspb.BinaryReader): Response;
+}
+
+export namespace Response {
+  export type AsObject = {
+    displayMessage: string,
+  }
+}
+
--- server/proto/check/check.pb.go
+++ server/proto/check/check.pb.go
@@ -7,10 +7,15 @@
 package check
 
 import (
+	context "context"
 	proto "github.com/golang/protobuf/proto"
+	grpc "google.golang.org/grpc"
+	codes "google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
+	sync "sync"
 )
 
 const (
@@ -24,18 +29,144 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
+type Request struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	None string `protobuf:"bytes,1,opt,name=none,proto3" json:"none,omitempty"`
+}
+
+func (x *Request) Reset() {
+	*x = Request{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_check_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Request) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Request) ProtoMessage() {}
+
+func (x *Request) ProtoReflect() protoreflect.Message {
+	mi := &file_check_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Request.ProtoReflect.Descriptor instead.
+func (*Request) Descriptor() ([]byte, []int) {
+	return file_check_proto_rawDescGZIP(), []int{0}
+}
+
+func (x *Request) GetNone() string {
+	if x != nil {
+		return x.None
+	}
+	return ""
+}
+
+type Response struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	DisplayMessage string `protobuf:"bytes,1,opt,name=display_message,json=displayMessage,proto3" json:"display_message,omitempty"`
+}
+
+func (x *Response) Reset() {
+	*x = Response{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_check_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Response) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Response) ProtoMessage() {}
+
+func (x *Response) ProtoReflect() protoreflect.Message {
+	mi := &file_check_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Response.ProtoReflect.Descriptor instead.
+func (*Response) Descriptor() ([]byte, []int) {
+	return file_check_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *Response) GetDisplayMessage() string {
+	if x != nil {
+		return x.DisplayMessage
+	}
+	return ""
+}
+
 var File_check_proto protoreflect.FileDescriptor
 
 var file_check_proto_rawDesc = []byte{
 	0x0a, 0x0b, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x05, 0x63,
-	0x68, 0x65, 0x63, 0x6b, 0x42, 0x09, 0x5a, 0x07, 0x2e, 0x3b, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x62,
-	0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x68, 0x65, 0x63, 0x6b, 0x22, 0x1d, 0x0a, 0x07, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12,
+	0x12, 0x0a, 0x04, 0x6e, 0x6f, 0x6e, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
+	0x6f, 0x6e, 0x65, 0x22, 0x33, 0x0a, 0x08, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12,
+	0x27, 0x0a, 0x0f, 0x64, 0x69, 0x73, 0x70, 0x6c, 0x61, 0x79, 0x5f, 0x6d, 0x65, 0x73, 0x73, 0x61,
+	0x67, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0e, 0x64, 0x69, 0x73, 0x70, 0x6c, 0x61,
+	0x79, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x32, 0x68, 0x0a, 0x06, 0x52, 0x6f, 0x75, 0x74,
+	0x65, 0x73, 0x12, 0x2e, 0x0a, 0x0b, 0x48, 0x65, 0x61, 0x6c, 0x74, 0x68, 0x43, 0x68, 0x65, 0x63,
+	0x6b, 0x12, 0x0e, 0x2e, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x1a, 0x0f, 0x2e, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
+	0x73, 0x65, 0x12, 0x2e, 0x0a, 0x0b, 0x53, 0x65, 0x63, 0x72, 0x65, 0x74, 0x43, 0x68, 0x65, 0x63,
+	0x6b, 0x12, 0x0e, 0x2e, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x2e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x1a, 0x0f, 0x2e, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x2e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e,
+	0x73, 0x65, 0x42, 0x09, 0x5a, 0x07, 0x2e, 0x3b, 0x63, 0x68, 0x65, 0x63, 0x6b, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
-var file_check_proto_goTypes = []interface{}{}
+var (
+	file_check_proto_rawDescOnce sync.Once
+	file_check_proto_rawDescData = file_check_proto_rawDesc
+)
+
+func file_check_proto_rawDescGZIP() []byte {
+	file_check_proto_rawDescOnce.Do(func() {
+		file_check_proto_rawDescData = protoimpl.X.CompressGZIP(file_check_proto_rawDescData)
+	})
+	return file_check_proto_rawDescData
+}
+
+var file_check_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_check_proto_goTypes = []interface{}{
+	(*Request)(nil),  // 0: check.Request
+	(*Response)(nil), // 1: check.Response
+}
 var file_check_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
+	0, // 0: check.Routes.HealthCheck:input_type -> check.Request
+	0, // 1: check.Routes.SecretCheck:input_type -> check.Request
+	1, // 2: check.Routes.HealthCheck:output_type -> check.Response
+	1, // 3: check.Routes.SecretCheck:output_type -> check.Response
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -46,21 +177,164 @@ func file_check_proto_init() {
 	if File_check_proto != nil {
 		return
 	}
+	if !protoimpl.UnsafeEnabled {
+		file_check_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Request); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_check_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Response); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_check_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   0,
+			NumMessages:   2,
 			NumExtensions: 0,
-			NumServices:   0,
+			NumServices:   1,
 		},
 		GoTypes:           file_check_proto_goTypes,
 		DependencyIndexes: file_check_proto_depIdxs,
+		MessageInfos:      file_check_proto_msgTypes,
 	}.Build()
 	File_check_proto = out.File
 	file_check_proto_rawDesc = nil
 	file_check_proto_goTypes = nil
 	file_check_proto_depIdxs = nil
+}
+
+// Reference imports to suppress errors if they are not otherwise used.
+var _ context.Context
+var _ grpc.ClientConnInterface
+
+// This is a compile-time assertion to ensure that this generated file
+// is compatible with the grpc package it is being compiled against.
+const _ = grpc.SupportPackageIsVersion6
+
+// RoutesClient is the client API for Routes service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
+type RoutesClient interface {
+	HealthCheck(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+	SecretCheck(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error)
+}
+
+type routesClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewRoutesClient(cc grpc.ClientConnInterface) RoutesClient {
+	return &routesClient{cc}
+}
+
+func (c *routesClient) HealthCheck(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/check.Routes/HealthCheck", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *routesClient) SecretCheck(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/check.Routes/SecretCheck", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// RoutesServer is the server API for Routes service.
+type RoutesServer interface {
+	HealthCheck(context.Context, *Request) (*Response, error)
+	SecretCheck(context.Context, *Request) (*Response, error)
+}
+
+// UnimplementedRoutesServer can be embedded to have forward compatible implementations.
+type UnimplementedRoutesServer struct {
+}
+
+func (*UnimplementedRoutesServer) HealthCheck(context.Context, *Request) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method HealthCheck not implemented")
+}
+func (*UnimplementedRoutesServer) SecretCheck(context.Context, *Request) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method SecretCheck not implemented")
+}
+
+func RegisterRoutesServer(s *grpc.Server, srv RoutesServer) {
+	s.RegisterService(&_Routes_serviceDesc, srv)
+}
+
+func _Routes_HealthCheck_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RoutesServer).HealthCheck(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/check.Routes/HealthCheck",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RoutesServer).HealthCheck(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _Routes_SecretCheck_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Request)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(RoutesServer).SecretCheck(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/check.Routes/SecretCheck",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(RoutesServer).SecretCheck(ctx, req.(*Request))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _Routes_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "check.Routes",
+	HandlerType: (*RoutesServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "HealthCheck",
+			Handler:    _Routes_HealthCheck_Handler,
+		},
+		{
+			MethodName: "SecretCheck",
+			Handler:    _Routes_SecretCheck_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "check.proto",
 }
