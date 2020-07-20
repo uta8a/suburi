f1b0c6d7c0efc0dcf8730ea2f5b5a076d14b4b59
uta8a <noumodulity@gmail.com>
2020-07-20 12:12:21 +0900

    Key from .env

--- server/auth/auth.go
+++ server/auth/auth.go
@@ -2,21 +2,26 @@ package handler
 
 import (
   "time"
+  "log"
   "os"
-	_ "context"
-	"github.com/dgrijalva/jwt-go"
+  _ "context"
+  "github.com/dgrijalva/jwt-go"
   pbuser "github.com/uta8a/suburi/server/proto/user"
 )
 
-const Tokenkey = "token"
-
 func CreateToken(username string) (string, error) {
+  
+  TokenSecret := os.Getenv("TOKEN_SECRET")
+  if TokenSecret == "" {
+    log.Fatal("Env is not set")
+  }
+  //log.Printf("ENV: %s", TokenSecret)
   token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
     "username": username,
     "iat": time.Now().Unix(),
     "exp": time.Now().Add(time.Hour * 24).Unix(),
   })
-  tokenString, err := token.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+  tokenString, err := token.SignedString([]byte(TokenSecret))
   if err != nil {
     return "", err
   }
