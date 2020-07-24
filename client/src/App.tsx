11a155f8e1e8423564850ba7157271896f247681
uta8a <noumodulity@gmail.com>
2020-07-25 06:00:50 +0900

    prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { Request } from './proto/user/user_pb';
-import { UserClient } from './proto/user/UserServiceClientPb';
-import useSWR from 'swr';
-import { URL } from './util/global'
-import { Login } from './components/Login'
-import { AllRoute } from './components/AllRoute'
-import { AllSwitch } from './components/AllSwitch'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React, { useState } from "react";
+import { Request } from "./proto/user/user_pb";
+import { UserClient } from "./proto/user/UserServiceClientPb";
+import useSWR from "swr";
+import { URL } from "./util/global";
+import { Login } from "./components/Login";
+import { AllRoute } from "./components/AllRoute";
+import { AllSwitch } from "./components/AllSwitch";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Header } from "./components/view/Header";
 
 interface ioState {
   // input
@@ -17,38 +18,38 @@ interface ioState {
 }
 
 const App = () => {
-  const [
-    { token, message },
-    setState,
-  ] = useState({ token: "", message: ""})
+  const [{ token, message }, setState] = useState({ token: "", message: "" });
   const onChange = () => {
     const request = new Request();
     request.setUsername("");
     request.setPassword("");
     const client = new UserClient(`${URL}`, {}, {});
-    client.getToken(request, null).then(ret => {
-      setState({ token: ret.getToken(), message: ret.getDisplayMessage() })
-    }).catch(err => {
-      throw err;
-    });
-  }
+    client
+      .getToken(request, null)
+      .then((ret) => {
+        setState({ token: ret.getToken(), message: ret.getDisplayMessage() });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
   const fetcher = () => {
     const request = new Request();
     request.setUsername("");
     request.setPassword("");
     const client = new UserClient(`${URL}`, {}, {});
-    const ret = client.getToken(request, null).catch(err => {
+    const ret = client.getToken(request, null).catch((err) => {
       throw err;
     });
     return ret;
-  }
-  const { data }= useSWR('/getToken', fetcher)
+  };
+  const { data } = useSWR("/getToken", fetcher);
   return (
     <Router>
-      <AllRoute />
+      <Header />
       <AllSwitch />
     </Router>
   );
-}
+};
 
 export default App;
--- client/src/util/global.tsx
+++ client/src/util/global.tsx
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:8080';
+const URL = "http://localhost:8080";
+const CTF_NAME = "ErilyCTF";
 
-
-export { URL };
+export { URL, CTF_NAME };
