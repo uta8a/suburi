09e4f0186ebccd8670c829d011a290491f04136b
uta8a <noumodulity@gmail.com>
2020-07-25 15:06:23 +0900

    Subtitle add

--- client/src/util/global.tsx
+++ client/src/util/global.tsx
@@ -1,6 +1,14 @@
+import React from 'react'
 const URL = "http://localhost:8080";
 const CTF_NAME = "ErilyCTF";
 const CTF_ADMIN_NAME = "@kaito_tateyama";
 const CTF_ADMIN_LINK = "https://twitter.com/kaito_tateyama";
+const CTF_SUBTITLE = () => {
+  return (
+    <>
+    🏁🏁🏁 これは試験的に行われます。フィードバックは<a href="#">こちら</a>でお待ちしています。 🏁🏁🏁
+    </>
+  )
+}
 
-export { URL, CTF_NAME, CTF_ADMIN_NAME, CTF_ADMIN_LINK };
+export { URL, CTF_NAME, CTF_ADMIN_NAME, CTF_ADMIN_LINK, CTF_SUBTITLE };
