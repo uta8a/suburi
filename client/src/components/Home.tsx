78609356cc3f0afc7ac4c713702021a903e9f9a3
uta8a <noumodulity@gmail.com>
2020-07-26 08:36:33 +0900

    Home change design bit

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import { CTF_NAME,CTF_SUBTITLE } from '../util/global'
+import React from "react";
+import { CTF_NAME, CTF_SUBTITLE } from "../util/global";
 const Home = () => {
-
   return (
     <div className="flex w-full h-full flex-col justify-center items-center">
-      <h1 className="my-6 text-center text-white text-5xl font-bold">{CTF_NAME}</h1>
-      <p className="my-6  text-white text-center"><CTF_SUBTITLE /></p>
-      <div className="ctf-timer my-6 text-white text-center text-4xl">0:00:00</div>
+      <h1 className="my-6 text-center text-white text-5xl font-bold">
+        {CTF_NAME}
+      </h1>
+      <p className="my-6  text-white text-center">
+        <CTF_SUBTITLE />
+      </p>
+      <div className="ctf-timer my-6 text-white text-center text-4xl">
+        0:00:00
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export { Home }
+export { Home };
