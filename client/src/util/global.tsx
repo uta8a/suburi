9d94e3de02cc55db701b65d4c2190a8272bd23d2
uta8a <noumodulity@gmail.com>
2020-07-26 08:36:08 +0900

    config

--- client/src/util/global.tsx
+++ client/src/util/global.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 const URL = "http://localhost:8080";
 const CTF_NAME = "ErilyCTF";
 const CTF_ADMIN_NAME = "@kaito_tateyama";
@@ -6,9 +6,13 @@ const CTF_ADMIN_LINK = "https://twitter.com/kaito_tateyama";
 const CTF_SUBTITLE = () => {
   return (
     <>
-    🏁🏁🏁 これは試験的に行われます。フィードバックは<a href="#">こちら</a>でお待ちしています。 🏁🏁🏁
+      🚩🚩🚩 これは試験的に行われます。フィードバックは
+      <a className="bg-green-500 rounded-md underline" href="#">
+        こちら
+      </a>
+      からお願いします。 🚩🚩🚩
     </>
-  )
-}
+  );
+};
 
 export { URL, CTF_NAME, CTF_ADMIN_NAME, CTF_ADMIN_LINK, CTF_SUBTITLE };
